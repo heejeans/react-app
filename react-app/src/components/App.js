c9bd51642b95cdf70620b84e322d321f62b4825c
Joy <joyh.son@gmail.com>
2021-03-25 02:25:52 -0400

    more fixes

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,62 +1,54 @@
-//import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
-import css from './App.module.css';
-import Navbar from './Navbar.js';
-import Header from './Header.js';
-import Home from './Home.js';
-import Explore from './Explore.js';
-import NewPost from './NewPost.js';
-import Activity from './Activity.js';
-import Profile from './Profile.js';
-import initialStore from './utils/initialStore';
-import uniqueId from './utils/uniqueId';
-//npm start, npm run deploy
+import React, { useState } from "react";
+import initialStore from "utils/initialStore";
+import "./App.css";
+import css from "./App.module.css";
+import Header from "./Header.js";
+import Home from "./Home.js";
+import Explore from "./Explore.js";
+import NewPost from "./NewPost.js";
+import Activity from "./Activity.js";
+import Profile from "./Profile.js";
+import Navbar from "./Navbar.js";
+import uniqueId from "utils/uniqueId.js";
 
 function App() {
-
-    const [page, setPage] = useState('home');
-    const [store, setStore] = useState(initialStore);
-
-  function addLike(postId){
+  const [page, setPage] = useState("home");
+  const [store, setStore] = useState(initialStore);
+  function addLike(postId) {
     const like = {
-        userId: store.currentUserId, 
-        postId, // make sure you understand this shorthand syntax
-        datetime: new Date().toISOString()
+      userId: store.currentUserId,
+      postId,
+      datetime: new Date().toISOString()
     };
-    
+
     setStore({
-        store:{
-          ...store,// spread props. make sure you understand this
-          likes: store.likes.concat(like)
-        }
+      ...store,
+      likes: store.likes.concat(like)
+    });
+  }
+  function removeLike(postId) {
+    // console.log('removeLike',postId, store.likes.filter(like=>!(like.userId===store.currentUserId && like.postId===postId)));
+    setStore({
+      ...store,
+      likes: store.likes.filter(
+        like => !(like.userId === store.currentUserId && like.postId === postId)
+      )
     });
   }
 
-  function removeLike(postId){
-    // use filter and currentUserId to remove the like from the likes array
-    setStore(state=>({
-      store:{
-        ...store,
-        likes: store.likes.filter(like=>!(like.userId===state.store.currentUserId && like.postId===postId))
-      }
-  }));
-  }
-
-  function addComment(postId, text){
+  function addComment(postId, text) {
     const comment = {
-      userId: store.currentUserId, 
+      userId: store.currentUserId,
       postId,
       text,
       datetime: new Date().toISOString()
     };
     setStore({
       ...store,
-        comments:store.comments.concat(comment)
+      comments: store.comments.concat(comment)
     });
   }
-
-  function addPost(photo, desc){
+  function addPost(photo, desc) {
     const post = {
       id: uniqueId("post"),
       userId: store.currentUserId,
@@ -68,42 +60,51 @@ function App() {
       ...store,
       posts: store.posts.concat(post)
     });
+    
     setPage("home");
   }
-  function cancelPost(){
+  function cancelPost() {
+    // TODO:
+    // 1. Call setPage to come back to the home page (we will use Router to improve this)
     setPage("home");
   }
-
-  function renderMain(page){
-    switch(page){
-    case "home": return <Home 
-    store={store}
-    onLike={addLike} 
-    onUnlike={removeLike}
-    onComment={addComment}  />
-    case "explore": return <Explore/>;
-    case "newpost": return <NewPost
-    store={store}
-    onPost={addPost}
-    onPostCancel={cancelPost}
-    />;
-    case "activity": return <Activity/>;
-    case "profile": return <Profile
-    store={store}
-    />
-    default: return <Home store={store} />;
+  function renderMain(page) {
+    switch (page) {
+      case "home":
+        return (
+          <Home
+            store={store}
+            onLike={addLike}
+            onUnlike={removeLike}
+            onComment={addComment}
+          />
+        );
+      case "explore":
+        return <Explore />;
+      case "activity":
+        return <Activity />;
+      case "newpost":
+        return (
+          <NewPost
+            store={store}
+            onPost={addPost}
+            onPostCancel={cancelPost}
+          />
+        );
+      case "profile":
+        return <Profile store={store} />;
+      default:
+        return <Home store={store} />;
     }
   }
-  
+
   return (
     <div className={css.container}>
-      <Header/>
-      <main className={css.content}>
-        {renderMain(page)} 
-      </main>
-      <Navbar onNavChange={setPage}/>
-    </div>);
-  
+      <Header />
+      <main className={css.content}>{renderMain(page)}</main>
+      <Navbar onNavChange={setPage} />
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/Post.js
+++ react-app/src/components/Post.js
@@ -1,68 +1,86 @@
-import React, { useState } from 'react';
-import css from './Post.module.css';
-import publicUrl from './utils/publicUrl.js';
-import timespan from './utils/timespan.js';
+import React, {useState} from "react";
+import css from "./Post.module.css";
+import timespan from "utils/timespan.js";
+import publicUrl from "utils/publicUrl.js";
 
-function Post(props){
-    // function renderLike(self){
-    //     if (!self){
-    //         return (<img src={publicUrl('/assets/assets/like.svg')} alt="Like"/>)
-    //     }
-    //     else{
-    //         return(<img src={publicUrl('/assets/assets/unlike.svg')} alt="Unlike"/>)
-    //     }
-    // }
-    const [comment, setComment] = useState('');
-    const [toggleComment, setToggleComment] = useState(false); // hidden initially
-    function handleLike(){ 
-        props.onLike(props.post.id);
-    }
-    function handleUnlike(){
-        props.onUnlike(props.post.id);
-    }
-    function handleSubmitComment(event){
-        props.onComment(props.post.id, comment); // this calls addComment from App.js
-        setComment(''); //reset
-        setToggleComment(false); //close comment box
-        event.preventDefault(); // prevent page refresh
-    }
-    return(
-        <div className={css.container}>
-            <div className={css.user}>
-                <img className={css.profile} src={publicUrl(props.user.photo)} alt="Pfp"/>
-                <div className={css.username}> <b>{props.user.id}</b></div>  
-            </div>
-            <div className={css.post}>
-                <img className={css.photo} src={publicUrl(props.post.photo)} id={props.post.id} alt="Pfp"/>
-                <div className={css.action}>
-                <button>
-	                {props.likes.self?
-		                <img onClick={handleUnlike} src={publicUrl('/assets/assets/unlike.svg')} alt='Unlike Action'/> :
-		                <img onClick={handleLike} src={publicUrl('/assets/assets/like.svg')} alt='Like Action'/> 
-	                }
-                </button>
-                    <div onClick={e=>setToggleComment(!toggleComment)} className={css.comment}>
-                        <img src={publicUrl('/assets/assets/comment.svg')} alt="Comment"/> 
-                    </div>
-                </div>
-                <div className={css.likes}> <b>{props.likes.count} likes</b></div>
-                <div className={css.caption}> <b>{props.post.userId}</b> {props.post.desc} </div>
-            </div>
-            <div className={css.commentsection}>
-                {props.comments.map(comment=>
-                    <div> <b>{comment.userId}</b> {comment.text} </div>
-                )}
-            </div>
-            <div className={css.date}> {timespan(props.post.datetime)} ago</div>
-            {toggleComment &&
-            <form className={css.addComment} onSubmit={handleSubmitComment}>
-                <input type="text" placeholder="Add a comment…" value={comment} onChange={e=>
-            setComment(e.target.value)}/>
-                <button type="submit">Post</button>
-            </form>}
-
-            
-        </div>
-    );
+function Post(props) {
+  const [comment, setComment] = useState('');
+  const [toggleComment, setToggleComment] = useState(false); // hidden initially
+  
+  function handleLike() {
+    props.onLike(props.post.id);
+  }
+  function handleUnlike() {
+    props.onUnlike(props.post.id);
+  }
+  function handleSubmitComment(event){
+  props.onComment(props.post.id, comment); // this calls addComment from App.js
+  setComment(''); //reset
+  setToggleComment(false); //close comment box
+  event.preventDefault(); // prevent page refresh
 }
+
+  return (
+    <article className={css.post}>
+      <header className={css.header}>
+        <button className={css.user} to={`/profile/${props.user.id}`}>
+          <img src={publicUrl(props.user.photo)} alt="User Profile" />
+          <span>{props.user.id} </span>
+        </button>
+      </header>
+      <section className={css.content}>
+        <div className={css.imgContainer}>
+          <img src={publicUrl(props.post.photo)} alt="Post" />
+        </div>
+      </section>
+
+      <section className={css.actions}>
+        <button>
+          {props.likes.self ? (
+            <img
+              src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Funlike.svg?v=1614382900439"
+              alt="Unlike Action"
+              onClick={handleUnlike}
+            />
+          ) : (
+            <img
+              src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Flike.svg?v=1614382900207"
+              alt="Like Action"
+              onClick={handleLike}
+            />
+          )}
+        </button>
+        <button  onClick={e=>setToggleComment(!toggleComment)}> 
+          <img
+            src="https://cdn.glitch.com/f70d577d-f905-45ee-bb92-042bfa767970%2Fcomment.svg?v=1614382900046"
+            alt="Comment Action"
+          />
+        </button>
+      </section>
+      <section className={css.activity}>
+        <div className={css.likes}>{props.likes.count} likes</div>
+        <div className={css.comments}>
+          <div>
+            <span>{props.post.userId}</span>
+            <span>{props.post.desc}</span>
+          </div>
+          {props.comments.map((comment, i) => (
+            <div key={i}>
+              <span>{comment.userId}</span>
+              <span>{comment.text}</span>
+            </div>
+          ))}
+        </div>
+        <time className={css.time}>
+          {timespan(props.post.datetime).toUpperCase()} AGO
+        </time>
+        {toggleComment && <form className={css.addComment} onSubmit={handleSubmitComment}>
+          <input type="text" placeholder="Add a commentâ€¦" value={comment} onChange={e=>setComment(e.target.value)}/>
+          <button type="submit">Post</button>
+      </form>}
+      </section>
+    </article>
+  );
+}
+
 export default Post;
